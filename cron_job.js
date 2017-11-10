f1f27b0a799bb6e018d22cb5a74f63371052dd65
Mark Meyer <mark@photo-mark.com>
2017-11-10 12:46:44 -0900

    add delay between twilio calls, rearrage a little to make testing easier, add test for sendMessages()

--- cron_job.js
+++ cron_job.js
@@ -28,35 +28,41 @@ var job = new cron.CronJob({
     timeZone: 'America/Anchorage'
 });
 
-
-function sendMessages() {
-    // instantiating this here so it'll run without an auth token in dev
-    var twilio_client = twilio(TWILIO_SID, TWILIO_AUTH_TOKEN)
+function sendMessages(cb) {
+    var DELAY = 500; // throttle calles to twilio
 
     // get weather
     forecast.getLowTemps(function(err, data) {
         if (err) return rollbar.handleError(err)
-
-        db('subscribers').forEach(function(subscriber) {
-            console.log(subscriber)
-            if (data[subscriber.zip] <= NOTIFICATION_TEMPERATURE) {
-                twilio_client.sendMessage(
-                    {
-                        to: subscriber.phone,
-                        from: TWILIO_NUMBER,
-                        body: randomElement(message_text.NOTIFICATIONS),
-                    },
-                    function (err, response) {
-                        if (err) return rollbar.handleError(err)
-
-                        console.log(response)
-                    }
-                )
+        var subscribers = db('subscribers').filter(sub => data[sub.zip] <= NOTIFICATION_TEMPERATURE)
+        // loop over subscribers with delay
+        var inter = setInterval( gen => {
+            var next = gen.next()
+            if (next.done) {
+                clearInterval(inter)
+                return cb()
             }
-        })
+            var subscriber = next.value
+            module.exports.sendToTwilio(subscriber.phone, randomElement(message_text.NOTIFICATIONS))
+        }, DELAY, subscribers[Symbol.iterator]())
     })
 }
 
+function sendToTwilio(to, body){
+    var twilio_client = twilio(TWILIO_SID, TWILIO_AUTH_TOKEN)
+    twilio_client.sendMessage(
+        {
+            to: subscriber.phone,
+            from: TWILIO_NUMBER,
+            body: randomElement(message_text.NOTIFICATIONS),
+        },
+        function (err, response) {
+            if (err) return rollbar.handleError(err)
+
+            console.log(response)
+        }
+    )
+}
 
 function randomElement(items) {
     return items[Math.floor(Math.random()*items.length)]
@@ -64,3 +70,4 @@ function randomElement(items) {
 
 module.exports.job = job
 module.exports.sendMessages = sendMessages
+module.exports.sendToTwilio = sendToTwilio
--- test/test.js
+++ test/test.js
@@ -2,10 +2,13 @@
 
 // 3rd party library imports
 var cron = require('cron')
+var sinon = require('sinon')
 var twilio = require('twilio')
+var fs = require('fs')
 
 // project imports
 var forecast = require('../temp_forecast')
+var cron_job = require('../cron_job')
 
 // Super basic test to make sure the messages.json file is valid
 require('../message_text.json')
@@ -20,4 +23,27 @@ describe("Hit the weather API", () => {
         done(err)
     })
   })
+describe("Sending Messages", () =>{
+    var sendStub, getTempStub
+    var tempObj = {
+        '99501': 16, '99502': 16, '99503': 40
+    }
+    beforeEach(function(){
+        sendStub = sinon.stub(cron_job, 'sendToTwilio')
+        getTempStub = sinon.stub(forecast, 'getLowTemps')
+        getTempStub.yields(null, tempObj)
+    })
+    afterEach(function(){
+        sendStub.restore()
+        getTempStub.restore()
+    })
+    it("Calls sendToTwilio", function(done) {
+        cron_job.sendMessages(function(){
+            sinon.assert.calledWith(sendStub, '+19075551111')
+            sinon.assert.calledWith(sendStub, '+19075551112')
+            sinon.assert.neverCalledWith(sendStub, '+19075551113')
+            done()
+        })
+    })
+})
 })
