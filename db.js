e04236e6238c0e2813f25f8a355f02438664cb16
Mark Meyer <mark@photo-mark.com>
2017-11-10 13:40:17 -0900

    add delay between twilio calls, rearrage a little to make testing easier, add test for sendMessages()

--- db.js
+++ db.js
@@ -4,5 +4,5 @@ var low = require('lowdb')
 var storage = require('lowdb/file-sync')
 
 var dbPath = process.env.NODE_ENV === 'test' ? './test/fixtures/db.json' : 'db.json'
-
+console.log("db path: ", dbpath)
 module.exports = low(dbPath, { storage: storage })
